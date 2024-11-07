e0093a1d1ff75944d9597c710e74045563ad1f2f
BoumeshouliYouness <boumeshouliyouness6@gmail.com>
2024-11-07 19:34:57 +0100

    create a server to use the api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,1 @@
-fetch("https://api.algobook.info/v1/watches/all").then(res=>res.json()).then(sa3a=> console.log(sa3a))
+fetch("http://localhost:3000/0").then(res=>res.json()).then(res=>console.log(res))    
