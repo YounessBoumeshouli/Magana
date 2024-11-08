9ac6f04dfe42037d71062d75cd11d6f716ad61fa
BoumeshouliYouness <boumeshouliyouness6@gmail.com>
2024-11-08 14:59:20 +0100

    modifier file data.json

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,2 @@
-fetch("http://localhost:3000/0").then(res=>res.json()).then(res=>console.log(res))    
+
+fetch("http://localhost:3000/1").then(res=>res.json()).then(res=>document.getElementById("test").innerHTML = `<img src="${res.products[0].image}">`)    
