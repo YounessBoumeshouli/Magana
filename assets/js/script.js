1862cc0bc5c37e9a2232a8346fce97bd84b4a475
BoumeshouliYouness <boumeshouliyouness6@gmail.com>
2024-11-09 13:17:45 +0100

    start working with tailwind

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
 // res[0].products[0].image
 fetch("http://localhost:3000/categories")
 .then(res=>res.json())
-.then(res=>document.getElementById("test").innerHTML=`<img src="${res[0].products[0].image}">`)    
+.then(res=>document.getElementById("test").innerHTML=`<img src="./assets/images/Product/${res[0].products[0].image}">`)    
