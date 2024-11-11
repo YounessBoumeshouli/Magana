bdacef0b23dc87bfc148a5506d18387770765dab
lakrouehamza <lakrounehamza10@gmail.com>
2024-11-11 16:52:07 +0100

    créer la header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,2 @@
-fetch("https://api.algobook.info/v1/watches/all").then(res=>res.json()).then(sa3a=> console.log(sa3a))
+fetch("https://api.algobook.info/v1/watches/all")
+.then(res=>res.json()).then(sa3a=> console.log(sa3a))
