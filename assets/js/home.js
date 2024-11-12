f7087e35193961d01dfb6006332b8ea6eb28e5f5
BoumeshouliYouness <boumeshouliyouness6@gmail.com>
2024-11-12 10:34:28 +0100

    ajouter API des Brands

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,62 +1,48 @@
-fetch("http://localhost:3000/categories")
-.then(res=>res.json())
-.then(res=>
-    res.forEach(categorie => {
-        document.getElementById("CategoriesContainer").innerHTML += `
-<div class="CategoryWatch">
-    <img src="./assets/images/Product/${categorie.products[0].image} ">
-</div>
-`
-    })
-
-    
-) 
-
 let slideIndex = 1;
 showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    
-  let i;
-  let slides = document.getElementsByClassName("CategoryWatch");
-  let dots = document.getElementsByClassName("dot");
-
-  if (n > slides.length) { slideIndex = 1; }
-  console.log(slides.length)
-  if (n < 1) { slideIndex = slides.length; }
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
 
-  if (slideIndex === 1) {
-    slides[0].style.display = "block";  
-    slides[slides.length - 1].style.display = "block"; 
-    slides[1].style.display = "block"; 
-  } else if (slideIndex === slides.length) {
-    slides[slideIndex - 1].style.display = "block";
-    slides[slideIndex - 2].style.display = "block";
-    slides[0].style.display = "block"; 
-  } else {
-    slides[slideIndex - 1].style.display = "block"; 
-    slides[slideIndex].style.display = "block"; 
-    slides[slideIndex + 1].style.display = "block"; 
-  }
 
-  dots[slideIndex - 1].className += " active";
+function showSlides(slideIndex) {
+fetch("http://localhost:3000/categories")
+.then(res=>res.json())
+.then(res=>
+
+  document.getElementById("CategoriesContainer").innerHTML = `
+  <div class="CategoryWatch">
+      <img src="./assets/images/Product/${res[slideIndex-1].products[0].image} " class="w-24">
+  </div>
+  <div class="CategoryWatch">
+      <img src="./assets/images/Product/${res[slideIndex].products[0].image} " class="w-24">
+  </div>
+  <div class="CategoryWatch">
+      <img src="./assets/images/Product/${res[slideIndex+1].products[0].image} " class="w-24">
+  </div>
+  `
+
+) 
   
   console.log("Current slide index:", slideIndex);
 }
+
+
+function ShowBrand(){
+  document.getElementById("BrandsContainer").innerHTML=""
+  fetch("http://localhost:5000/brand")
+  .then(res=>res.json()
+  .then(res=>
+    res[0].array.forEach(element => {
+     
+      console.log(element)
+    });
+  )
+  )
+ 
+  
+}
+ShowBrand()
+
